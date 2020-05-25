66be0bd4f5a618f8569ea0ff300660550ab792ab
mynameiszsofia <lakatos.zsofia1993@gmail.com>
2020-05-25 20:49:46 +0200

    ...

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Grayscale', // <title>
+  siteTitle: 'I\'m a Front-end web developer', // <title>
   manifestName: 'Grayscale',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
+  manifestIcon: 'src/assets/images/bike.png',
   pathPrefix: `/gatsby-starter-grayscale/`, // This path is subpath of your hosting https://domain/portfolio
   heading: `Zsofia Lakatos`,
   subHeading: 'I\'m a Front-end web developer. I build web applications  ',
